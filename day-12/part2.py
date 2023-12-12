7c56d995daa675d530f759b2d31c30b77af9af94
Tomás Lopes <1211289@isep.ipp.pt>
2023-12-12 21:49:42 +0000

    added day 12

---
+++ day-12/part2.py
@@ -0,0 +1,46 @@
+FILE = "input.txt"
+
+memo = {}
+
+def count(arrangement, nums):
+    k = (arrangement, tuple(nums))
+    if k in memo:
+        return memo[k]
+
+    if arrangement == "":
+        return 1 if len(nums) == 0 else 0
+
+    if len(nums) == 0:
+        return 0 if "#" in arrangement else 1
+
+    sum = 0
+
+    if arrangement[0] in ".?":
+        sum += count(arrangement[1:], nums)
+
+    n = nums[0]
+
+    if arrangement[0] in "#?":
+        if n <= len(arrangement) and "." not in arrangement[:n] and (n == len(arrangement) or arrangement[n] != "#"):
+            sum += count(arrangement[n+1:], nums[1:])
+
+    memo[k] = sum
+    return sum
+
+with open(FILE, "r") as file:
+    lines = file.readlines()
+    
+    sum = 0
+    for line in lines:
+        line = line.strip()
+        if line == "":
+            continue
+        arrangement = (line.split()[0] + '?') * 5
+        arrangement = arrangement[:-1] # remove last '?'
+        numbers = line.split()[1].strip().split(",")
+        numbers = [int(n) for n in numbers] * 5
+
+
+        res = count(arrangement, numbers)
+        sum += res
+    print(sum)
